06829d555cb519d5a43bdf2457f26494fc7304f2
Mohanapriya <mohanapriyap@stgit.com>
2023-07-13 19:27:50 +0530

    pod file changed

--- Pods/Target Support Files/Pods-iOS-native-code-iOS-native-codeUITests/Pods-iOS-native-code-iOS-native-codeUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_iOS_native_code_iOS_native_codeUITests : NSObject
-@end
-@implementation PodsDummy_Pods_iOS_native_code_iOS_native_codeUITests
-@end
